79d58745ad24b4a9a7030994042f85a5c825cf18
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-09-19 19:41:17 -0300

    feat: Sign In exeption flow 3a pass the test

--- src/signInUseCase.ts
+++ src/signInUseCase.ts
@@ -1,88 +1,118 @@
-type AuthorizationResult = {
-  user: AdminUser | null;
-};
-type AuthListener = (authResult: AuthorizationResult) => void;
+type Credentials = { email: string; password: string };
 
 type AdminUser = {
+  uid: string;
   username: string;
   email: string;
   photoUrl: string;
 };
+type AdimAccount = AdminUser & {
+  password: string;
+};
 
-type AppState = { user: AdminUser | null };
+type AppState = { user: AdminUser | null; warningMessage: string | null };
 
 type AppConfig = {
   initialState?: AppState;
-  authProvider: AuthProvider;
+  authProvider?: AuthProvider;
 };
 
 type FakeProviderConfig = {
-  enabledUsers?: AdminUser[];
-  requestSignInFor: string;
+  enabledUsers: AdimAccount[];
 };
 
 interface AuthProvider {
-  addAuthStateListener: (listener: AuthListener) => void;
-  signInUser: () => void;
+  signInWithCredentials: (credentials: Credentials) => Promise<AdminUser>;
 }
 
 interface AdminApp {
+  setUser: (user: AdminUser) => void;
+  setWarningMessage: (message: string) => void;
   getState: () => AppState;
 }
 
-export function configureAdminApp(config: AppConfig): AdminApp {
-  const state: AppState = config.initialState || { user: null };
+export function configureAdminApp(config?: AppConfig): AdminApp {
+  const initialState = { user: null, warningMessage: null };
+  const state: AppState = config?.initialState || initialState;
 
-  const onAuthSateChange = (authResult: AuthorizationResult) => {
-    const { user } = authResult;
-    setUser(user);
-  };
-
-  config.authProvider.addAuthStateListener(onAuthSateChange);
-
-  const setUser = (user: AdminUser | null) => {
+  const setUser = (user: AdminUser) => {
     state.user = user;
   };
+
+  const setWarningMessage = (message: string) => {
+    state.warningMessage = message;
+  };
+
   const getState = () => ({ ...state });
 
   return {
+    setUser,
+    setWarningMessage,
     getState,
   };
 }
 
-export function onUserSignInUseCase(authProvider: AuthProvider) {
-  authProvider.signInUser();
+export async function onUserSignInUseCase(
+  app: AdminApp,
+  authProvider: AuthProvider,
+  crendentials: Credentials
+) {
+  try {
+    const user = await authProvider.signInWithCredentials(crendentials);
+    app.setUser(user);
+  } catch (error) {
+    // handle potential errors
+    app.setWarningMessage(
+      "You do not have permission to enter the application. Check your email and password."
+    );
+  }
 }
 
 export function selectAdimnUser(app: AdminApp): AdminUser | null {
   return app.getState().user;
 }
+export function selectWarningMessage(app: AdminApp): string | null {
+  return app.getState().warningMessage;
+}
 
 export function fakeAuthProvider(config: FakeProviderConfig): AuthProvider {
-  const email = config.requestSignInFor;
-  const authStateListeners = new Array<AuthListener>();
+  const { enabledUsers } = config;
 
-  function authorizeUserByEmail(emailToAuthorize: string): AuthorizationResult {
-    const user = config.enabledUsers?.find(
-      (enableUser) => enableUser.email === emailToAuthorize
-    );
-    if (user) {
-      return { user };
+  function findAccountByEmail(email: string) {
+    return enabledUsers.find((user) => email === user.email);
+  }
+
+  function getValidUser(credentials: Credentials) {
+    const { email: emailToCheck, password: passToCheck } = credentials;
+    const existingAccount = findAccountByEmail(emailToCheck);
+    if (existingAccount && existingAccount.password === passToCheck) {
+      const validUser = {
+        uid: existingAccount.uid,
+        username: existingAccount.username,
+        email: existingAccount.email,
+        photoUrl: existingAccount.photoUrl,
+      };
+      return validUser;
     }
-    return { user: null };
+    throw new AuthError("auth/credentials are invalid");
   }
 
-  function addAuthStateListener(authListener: AuthListener) {
-    authStateListeners.push(authListener);
-  }
-
-  function signInUser() {
-    const result = authorizeUserByEmail(email);
-    authStateListeners.forEach((listener) => listener(result));
+  async function signInWithCredentials(credentials: Credentials) {
+    try {
+      const validAccount = getValidUser(credentials);
+      return Promise.resolve(validAccount);
+    } catch (error) {
+      return Promise.reject({ message: "auth/credentials are invalid" });
+    }
   }
 
   return {
-    addAuthStateListener,
-    signInUser,
+    signInWithCredentials,
   };
+}
+
+class AuthError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
 }
--- src/test/signInUseCase.test.ts
+++ src/test/signInUseCase.test.ts
@@ -4,36 +4,81 @@ import {
   fakeAuthProvider,
   onUserSignInUseCase,
   selectAdimnUser,
+  selectWarningMessage,
 } from "../signInUseCase";
 
-describe("Sign In with Google Use Case", () => {
-  const anAdminUser = {
-    username: "Leo Quiroga",
+describe("Sign In Use Case", () => {
+  const enabledUsers = [
+    {
+      uid: "rt4-5eiug-uyt5",
+      email: "leoquiroga@gmail.com",
+      username: "Leo Quiroga",
+      password: "leo2022",
+      photoUrl: "https://potho-leo.io",
+    },
+    {
+      uid: "n98-poh7t-98u",
+      username: "Vasco Indaburu",
+      email: "indabumotorstore@gmail.com",
+      password: "store2022",
+      photoUrl: "https://potho-vasco.io",
+    },
+  ];
+  const validCredentetials = {
     email: "leoquiroga@gmail.com",
-    photoUrl: "https://potho-leo.io",
+    password: "leo2022",
+  };
+  const invalidCredentials = {
+    email: "nonenabled@gmail.com",
+    password: "blablebli",
   };
 
-  it("Should logged-in a user that already exist in the list of enabled users", () => {
-    // given a google provider that already has a user enabled
-    // and the admin will sign in with that user
+  it("Should logged-in a user that already exist in the list of enabled users", async () => {
+    // given an auth provider that already has a user enabled
     const authProvider = fakeAuthProvider({
-      enabledUsers: [anAdminUser],
-      requestSignInFor: anAdminUser.email,
+      enabledUsers,
     });
 
     // given an admin-app without a logged-in user
-    const adminApp = configureAdminApp({
-      authProvider: authProvider,
-    });
+    const adminApp = configureAdminApp();
 
     // expected result
-    const expectedAdminUser = anAdminUser;
+    const expectedAdminUser = {
+      uid: enabledUsers[0].uid,
+      email: enabledUsers[0].email,
+      username: enabledUsers[0].username,
+      photoUrl: enabledUsers[0].photoUrl,
+    };
 
     // use case
-    onUserSignInUseCase(authProvider);
+    await onUserSignInUseCase(adminApp, authProvider, validCredentetials);
 
     const adminUser = selectAdimnUser(adminApp);
 
     expect(adminUser).to.deep.equal(expectedAdminUser);
+  });
+
+  it("Should inform the user that he/she does not have permission to log in", async () => {
+    // given an auth provider that already has a user enabled
+    const authProvider = fakeAuthProvider({
+      enabledUsers,
+    });
+
+    // given an admin-app without a logged-in user
+    const adminApp = configureAdminApp();
+
+    // expected result
+    const expectedAdminUser = null;
+    const expectedMessage =
+      "You do not have permission to enter the application. Check your email and password.";
+
+    // use case
+    await onUserSignInUseCase(adminApp, authProvider, invalidCredentials);
+
+    const adminUser = selectAdimnUser(adminApp);
+    const warningMessage = selectWarningMessage(adminApp);
+
+    expect(adminUser).to.deep.equal(expectedAdminUser);
+    expect(warningMessage).to.equal(expectedMessage);
   });
 });
